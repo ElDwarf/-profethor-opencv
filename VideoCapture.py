9c8642d712cf0efe45aa4895ddb1f41ae0c3869e
Juan Manuel Aleman <juale86@gmail.com>
2019-03-10 12:28:24 -0300

    Se quita el imwrite

--- VideoCapture.py
+++ VideoCapture.py
@@ -6,7 +6,6 @@ cap = cv2.VideoCapture(0)
 while(True):
     ret, frame = cap.read()
     if ret:
-    	cv2.imwrite('videìto.mp4')
         cv2.imshow('video', frame)
     if cv2.waitKey(1) & 0xFF == 27:
         break
